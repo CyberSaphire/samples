68f4038665f8003e2bec4f9823eb8c51195a108d
justin <justin.jiang@outlook.com>
2016-03-20 11:17:12 +0800

    vso git initial commit

---
+++ VsoGit/VsoGit/VsoRestClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace VsoGit
+{
+    public class VsoRestClient
+    {
+        public static async void GetRepos(string username, string password, string requestUri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(
+                        System.Text.ASCIIEncoding.ASCII.GetBytes(
+                            string.Format("{0}:{1}", username, password))));
+
+                using (HttpResponseMessage response = client.GetAsync(
+                            requestUri).Result)
+                {
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+            }
+        }
+    }
+}
